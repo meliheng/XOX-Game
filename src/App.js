95264dd80bb987207b40ce02e5842a03a717719e
meliheng <melihcanyildiz9@gmail.com>
2022-08-23 20:31:10 +0300

    control row equality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+	const [mark, setMark] = useState('X');
+
+	useEffect(() => {
+		for (let i = 1; i <= 3; i++) {
+			const tdElements = document.querySelectorAll(`tr[id^="${i}"] td`);
+			if (
+				tdElements[0].innerHTML === tdElements[1].innerHTML &&
+				tdElements[1].innerHTML === tdElements[2].innerHTML
+			) {
+				console.log('winnn');
+				return;
+			}
+		}
+	}, [mark]);
+	const writeMark = (e) => {
+		e.target.innerHTML = mark;
+		setMark((prevState) => {
+			console.log(prevState);
+			return prevState === 'X' ? 'O' : 'X';
+		});
+	};
 	return (
 		<div className='App'>
 			<table>
 				<tbody>
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
+					<tr id='1'>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
 					</tr>
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
+					<tr id='2'>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
 					</tr>
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
+					<tr id='3'>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
+						<td onClick={(e) => writeMark(e)}></td>
 					</tr>
 				</tbody>
 			</table>
