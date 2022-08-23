9275a9db620f31c5a6fe9e2d35b71117d81b10f7
meliheng <melihcanyildiz9@gmail.com>
2022-08-23 19:09:58 +0300

    implement css for styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<div className='App'>
+			<table>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
